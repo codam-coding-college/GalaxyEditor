ba62ed29aa58dfb49059beeb27073044e125bcda
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-08-23 13:38:15 +0200

    Properly setup app context

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/25 11:01:54 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/08/23 10:25:11 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/08/23 13:37:47 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,48 @@ import { NameIDCallbackFunction, Project } from "./Utilities/Types";
 import { createContext, useContext, useState } from "react";
 
 ////////////////////////////////////////////////////////////////////////////////
-
-/**
- * The toolbar context which lets you hook onto
- */
+// APP Context
 interface AppContextType {
-	// Toolbar hooks
-    onSearchChange: (callback: NameIDCallbackFunction) => void;
-    onCursusChange: (callback: NameIDCallbackFunction) => void;
-    onCampusChange: (callback: NameIDCallbackFunction) => void;
-
-	// Data
 	projects: Project[];
+
+	
 }
 
 const AppContext = createContext<AppContextType>(null!);
+
+/**
+ * Data that is shared amongst the app.
+ * @param param0 The children in scope of this data.
+ * @returns The App context provider.
+ */
+const AppDataProvider = ({ children }: { children: React.ReactNode }) => {
+	// TODO: Set initial state to what was fetched from intra or is in cache.
+    const [projectData, setProjectData] = useState<Project[]>(null!);
+
+	// Construct the object
+    const value: AppContextType = { projects: projectData };
+
+    return (<AppContext.Provider value={value}>{children}</AppContext.Provider>);
+};
+
+// Retrieves the current App data.
+const GetAppData = () => {
+	return useContext(AppContext);
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 
 function App() {
     return (
         <>
-			<header>
-				<Toolbar />
-			</header>
-			<main>
-				<Canvas />
-			</main>
+			<AppDataProvider>
+				<header>
+					<Toolbar />
+				</header>
+				<main>
+					<Canvas />
+				</main>
+			</AppDataProvider>
         </>
     );
 }
